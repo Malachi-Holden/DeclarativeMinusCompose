8aeb3a49b079ce67e4c14da99bf8106c042b81bb
Malachi <malachi.coqui@gmail.com>
2022-12-24 23:44:08 -0600

    make lazylist from recyclerview

--- app/src/main/java/com/example/declarativeminuscompose/MainActivity.kt
+++ app/src/main/java/com/example/declarativeminuscompose/MainActivity.kt
@@ -1,39 +1,29 @@
 package com.example.declarativeminuscompose
 
 import android.os.Bundle
-import androidx.lifecycle.MutableLiveData
 import com.example.declarativeminuscompose.exComp.*
-import com.example.declarativeminuscompose.exComp.coreWidgets.Button
-import com.example.declarativeminuscompose.exComp.coreWidgets.HorizontalLayout
-import com.example.declarativeminuscompose.exComp.coreWidgets.VerticalLayout
-import com.example.declarativeminuscompose.exComp.coreWidgets.Text
+import com.example.declarativeminuscompose.exComp.coreWidgets.*
 
 class MainActivity : ExCompActivity() {
-    val x = MutableLiveData(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            x.bind()
-            InnerData(x.value, { x.value = it })
+            LazyList(Orientation.Horizontal) {
+                item {
+                    Text("hello")
+                }
+                item{
+                    Button({}, "goodbye")
+                }
+                repeat(100){
+                    item{
+                        Text("again: $it")
+                    }
+                }
+            }
 
         }
-    }
-}
-
-fun ExComp.InnerData(x: Int?, onChange: (Int?)->Unit){
-    VerticalLayout {
-        Text("value: $x", modifier = Modifier().textSize(30f))
-        Text("value again: $x", modifier = Modifier().textSize(30f))
-        HorizontalLayout {
-            Text("the button:", modifier = Modifier().textSize(30f))
-            Button(
-                modifier = Modifier().textSize(30f),
-                onClick = { onChange( x?.plus(1)) },
-                text = "increment"
-            )
-        }
-
     }
 }
---
+++ app/src/main/java/com/example/declarativeminuscompose/exComp/coreWidgets/LazyList.kt
@@ -0,0 +1,57 @@
+package com.example.declarativeminuscompose.exComp.coreWidgets
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.declarativeminuscompose.exComp.ExComp
+
+enum class Orientation{
+    Horizontal,
+    Vertical
+}
+
+fun ExComp.LazyList(orientation: Orientation = Orientation.Vertical, content: LazyListScope.()->Unit){
+    val layoutOrientation = when(orientation){
+        Orientation.Horizontal -> LinearLayout.HORIZONTAL
+        Orientation.Vertical -> LinearLayout.VERTICAL
+    }
+    Layout({
+        RecyclerView(this).apply {
+            layoutManager = LinearLayoutManager(this.context).apply { this.orientation = layoutOrientation }
+            adapter = LazyListAdapter(LazyListScope(this@LazyList).apply(content))
+        }
+    }){}
+}
+
+class LazyListScope(val parent: ExComp){
+    val children = mutableListOf<ExComp>()
+    fun item(content: ExComp.()->Unit){
+        children.add(ExComp(parent.lifecycleOwner, parent.observer) { FrameLayout(this) }.apply(content))
+    }
+}
+
+class LazyListAdapter(val scope: LazyListScope): Adapter<LazyListAdapter.LazyListViewHolder>(){
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int): LazyListViewHolder = LazyListViewHolder(FrameLayout(parent.context))
+
+    override fun onBindViewHolder(holder: LazyListViewHolder, position: Int) {
+        holder.bind(scope.children[position])
+    }
+
+    override fun getItemCount(): Int = scope.children.size
+
+    class LazyListViewHolder(val root: ViewGroup): ViewHolder(root) {
+        fun bind(exComp: ExComp){
+            root.removeAllViews()
+            root.addView(exComp.buildView(root.context))
+        }
+
+    }
+}
