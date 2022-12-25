039feb7d0e1dc4a87a0a05eebfacd1797b4895f8
Malachi <malachi.coqui@gmail.com>
2022-12-24 22:44:21 -0600

    update example activity

--- app/src/main/java/com/example/declarativeminuscompose/MainActivity.kt
+++ app/src/main/java/com/example/declarativeminuscompose/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.declarativeminuscompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
 import com.example.declarativeminuscompose.exComp.*
 import com.example.declarativeminuscompose.exComp.coreWidgets.Button
+import com.example.declarativeminuscompose.exComp.coreWidgets.HorizontalLayout
 import com.example.declarativeminuscompose.exComp.coreWidgets.VerticalLayout
 import com.example.declarativeminuscompose.exComp.coreWidgets.Text
 
@@ -16,19 +16,24 @@ class MainActivity : ExCompActivity() {
 
         setContent {
             x.bind()
-            VerticalLayout {
-                InnerData(x)
-            }
+            InnerData(x.value, { x.value = it })
 
         }
     }
 }
 
-fun ExComp.InnerData(x: MutableLiveData<Int>){
-    Text("value: ${x.value}", modifier = Modifier().textSize(30f))
-    Button(
-        modifier = Modifier().textSize(30f),
-        onClick = { x.value = x.value?.plus(1) },
-        text = "increment"
-    )
+fun ExComp.InnerData(x: Int?, onChange: (Int?)->Unit){
+    VerticalLayout {
+        Text("value: $x", modifier = Modifier().textSize(30f))
+        Text("value again: $x", modifier = Modifier().textSize(30f))
+        HorizontalLayout {
+            Text("the button:", modifier = Modifier().textSize(30f))
+            Button(
+                modifier = Modifier().textSize(30f),
+                onClick = { onChange( x?.plus(1)) },
+                text = "increment"
+            )
+        }
+
+    }
 }
