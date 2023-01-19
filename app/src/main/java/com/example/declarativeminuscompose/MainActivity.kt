6419b427c1d8e251ae9cea9d39075ed8d056463e
Malachi <malachi.coqui@gmail.com>
2023-01-18 20:31:21 -0600

    refactor example

--- app/src/main/java/com/example/declarativeminuscompose/MainActivity.kt
+++ app/src/main/java/com/example/declarativeminuscompose/MainActivity.kt
@@ -2,11 +2,9 @@ package com.example.declarativeminuscompose
 
 import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
+import com.holden.declarativeminuscompose.exComp.ExComp
 import com.holden.declarativeminuscompose.exComp.ExCompActivity
-import com.holden.declarativeminuscompose.exComp.coreWidgets.LazyList
-import com.holden.declarativeminuscompose.exComp.coreWidgets.Slider
-import com.holden.declarativeminuscompose.exComp.coreWidgets.Text
-import com.holden.declarativeminuscompose.exComp.coreWidgets.VerticalLayout
+import com.holden.declarativeminuscompose.exComp.coreWidgets.*
 
 class MainActivity : ExCompActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +16,12 @@ class MainActivity : ExCompActivity() {
     }
 }
 
-fun com.holden.declarativeminuscompose.exComp.ExComp.SliderListView(){
+fun ExComp.SliderListView(){
     val sliderVal = remember(MutableLiveData(0.1f))
     sliderVal.bind()
     VerticalLayout {
         Slider(value = sliderVal.value ?: 0f, onValueChange = { sliderVal.value = it })
-        LazyList(orientation= com.holden.declarativeminuscompose.exComp.coreWidgets.Orientation.Vertical){
+        LazyList(orientation= Orientation.Vertical){
             repeat(percentile(sliderVal.value)){
                 item {
                     Text("row $it")
